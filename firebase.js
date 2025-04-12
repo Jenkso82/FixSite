6b33f6f8c0791924057524ee98c323a7a9260559
Jenkso82 <shadomvd@gmail.com>
2025-04-13 00:30:11 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBQM63yuwWpfS6Fw373pYioMn5H2cP-M4s",
@@ -15,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { db, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
+export { db, auth, storage, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut };
